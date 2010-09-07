a0c6bd12701ae9bc45c1aa2c08315890458c5d8a
danielfranca <daniel.franca@gmail.com>
2010-09-07 19:48:23 -0300

    .

---
+++ urls.py
@@ -0,0 +1,43 @@
+# -*- coding: iso-8859-1 -*-
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
+from django.contrib import admin
+
+#from bookmarks.feeds import BookmarkFeed
+#bookmarks_feed_dict = {"feed_dict": { '': BookmarkFeed }}
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    url(r'^$', 'profiles.views.home', name='home'),
+
+    (r'^about/', include('about.urls')),
+    (r'^account/', include('account.urls')),
+    
+    #(r'^registration/', include('registration.urls')),
+    (r'^bbauth/', include('bbauth.urls')),
+    (r'^authsub/', include('authsub.urls')),
+    (r'^profiles/', include('profiles.urls')),
+    (r'^blog/', include('blog.urls')),
+    (r'^tags/', include('tag_app.urls')),
+    (r'^notices/', include('notification.urls')),
+    #(r'^announcements/', include('announcements.urls')),
+    (r'^comments/', include('threadedcomments.urls')),
+    (r'^robots.txt$', include('robots.urls')),
+    (r'^i18n/', include('django.conf.urls.i18n')),
+    (r'^follow/', include('follow.urls')),
+    (r'^admin/(.*)', admin.site.root),
+    (r'^publications/', include('publications.urls')),
+    (r'^avatar/', include('avatar.urls')),
+    (r'^flag/', include('flag.urls')),
+    
+    #(r'^feeds/bookmarks/(.*)/?$', 'django.contrib.syndication.views.feed', bookmarks_feed_dict),
+)
+
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns('', 
+        (r'^site_media/(?P<path>.*)$', 'misc.views.serve')
+    )
