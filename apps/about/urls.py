b67efa0b27fdebda24ea52dcc82370f7c9c0798c
danielfranca <danielfranca@danielfranca-MacBookPro.(none)>
2011-02-05 03:05:27 -0200

    Redirect page when access from IE6/IE7

--- apps/about/urls.py
+++ apps/about/urls.py
@@ -3,12 +3,12 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {"template": "about/about.html"}, name="about"),
-    
+
     url(r'^terms/$', direct_to_template, {"template": "about/terms.html"}, name="terms"),
     url(r'^privacy/$', direct_to_template, {"template": "about/privacy.html"}, name="privacy"),
     url(r'^dmca/$', direct_to_template, {"template": "about/dmca.html"}, name="dmca"),
-    
+
     url(r'^whats_tintz/$', direct_to_template, {"template": "about/whats_tintz.html"}, name="whats_tintz"),
-    url(r'^confirm_email/$', direct_to_template, {"template": "about/confirm_email.html"}, name="confirm_email"),
-    
+    url(r'^old_browser/$', direct_to_template, {"template": "about/old_browser.html"}, name="old_browser"),
+
 )
--- apps/about/views.py
+++ apps/about/views.py
@@ -1,1 +1,30 @@
+import re
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from tintz import settings
+
 # Create your views here.
+class RedirectBrowser:
+    """
+    Redirect old browsers (IE sux)
+    """
+    def process_request(self, request):
+
+        #import pdb; pdb.set_trace()
+
+        if request.path.startswith(settings.MEDIA_URL) or request.path.startswith("/about/old_browser/"):
+            return None
+
+        if request.META.has_key('HTTP_USER_AGENT'):
+            user_agent = request.META['HTTP_USER_AGENT']
+
+            # Test common mobile values.
+            pattern = "(MSIE 6|MSIE 7)"
+            prog = re.compile(pattern, re.IGNORECASE)
+            match = prog.search(user_agent)
+
+            if match:
+                return HttpResponseRedirect(reverse('old_browser'))
+            else:
+                return None
+
